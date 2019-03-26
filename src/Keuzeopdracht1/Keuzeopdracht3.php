460f46842997d468394e1d0ca09106c80e7da4fd
hicham <hicham.harmaz1@student.rocvf.nl>
2019-03-26 15:49:23 +0100

    Initial commit

---
+++ src/Keuzeopdracht1/Keuzeopdracht3.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_POST["submit"])) {
+    if (isset($_POST["subject"])) {
+        foreach ($_POST['subject'] as $subject)
+            echo '<img src="IMG/' . $subject . '.jpg">';
+    } else
+        echo "Select a car of your choosing!";
+}
+?>
+
+<html lang="en">
+<body>
+<form method='post'>
+    <select name='subject[]' multiple size=4>
+        <option value='Ferrari'>Ferrari</option>
+        <option value='Lambo'>Lambo</option>
+        <option value='Audi'>Audi</option>
+        <option value='VW'>VW</option>
+
+    </select>
+    <input type='submit' name='submit' value=Submit>
+</form>
+</body>
+</html>
